c50437142ccb830b7ef7121c0b28ca6e7e7a383e
sammy ochabs <sammymadgenius@gmail.com>
2020-11-19 02:20:47 +0100

    dd

--- pages/admin/add-post.js
+++ pages/admin/add-post.js
@@ -92,6 +92,16 @@ const AddPost = ({ posts }) => {
               rel="stylesheet"
               href="/devblog/icofont/icofont.min.css"
             ></link>
+            <script
+              src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+              integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+              crossorigin="anonymous"
+            ></script>
+            <script
+              src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+              integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+              crossorigin="anonymous"
+            ></script>
           </Head>
           <div className={toggleClass} id="wrapper">
             <div className="bg-dark border-right" id="sidebar-wrapper">
@@ -308,6 +318,16 @@ const AddPost = ({ posts }) => {
                 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
                 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
                 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+                <script
+                  src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                  crossorigin="anonymous"
+                ></script>
+                <script
+                  src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+                  integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+                  crossorigin="anonymous"
+                ></script>
               </Head>
               <div className={toggleClass} id="wrapper">
                 <div className="bg-dark border-right" id="sidebar-wrapper">
@@ -538,6 +558,16 @@ const AddPost = ({ posts }) => {
                   rel="stylesheet"
                   href="/devblog/icofont/icofont.min.css"
                 ></link>
+                <script
+                  src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                  crossorigin="anonymous"
+                ></script>
+                <script
+                  src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+                  integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+                  crossorigin="anonymous"
+                ></script>
               </Head>
               <div className={toggleClass} id="wrapper">
                 <div className="bg-dark border-right" id="sidebar-wrapper">
