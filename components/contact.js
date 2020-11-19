8824b8ac8366220b69cadf9383bc864b1136df73
sammy ochabs <sammymadgenius@gmail.com>
2020-11-20 00:02:58 +0100

    working...

--- components/contact.js
+++ components/contact.js
@@ -40,7 +40,7 @@ const Contact = () => {
               <i className="ion-ios-telephone-outline"></i>
               <h3>Phone Number</h3>
               <p>
-                <a href="tel:+155895548855"> (303) 519-8232</a>
+                <a href="tel:(303) 519-8232"> (303) 519-8232</a>
               </p>
             </div>
           </div>
@@ -50,82 +50,13 @@ const Contact = () => {
               <i className="ion-ios-email-outline"></i>
               <h3>Email</h3>
               <p>
-                <a href="mailto:info@example.com">
+                <a href="mailto:VirtualMgmtResources@gmail.com">
                   {" "}
                   VirtualMgmtResources@gmail.com
                 </a>
               </p>
             </div>
           </div>
-        </div>
-
-        <div className="form">
-          <form
-            action="forms/contact.php"
-            method="post"
-            role="form"
-            className="php-email-form"
-          >
-            <div className="form-row">
-              <div className="form-group col-md-6">
-                <input
-                  type="text"
-                  name="name"
-                  className="form-control"
-                  id="name"
-                  placeholder="Your Name"
-                  data-rule="minlen:4"
-                  data-msg="Please enter at least 4 chars"
-                />
-                <div className="validate"></div>
-              </div>
-              <div className="form-group col-md-6">
-                <input
-                  type="email"
-                  className="form-control"
-                  name="email"
-                  id="email"
-                  placeholder="Your Email"
-                  data-rule="email"
-                  data-msg="Please enter a valid email"
-                />
-                <div className="validate"></div>
-              </div>
-            </div>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                name="subject"
-                id="subject"
-                placeholder="Subject"
-                data-rule="minlen:4"
-                data-msg="Please enter at least 8 chars of subject"
-              />
-              <div className="validate"></div>
-            </div>
-            <div className="form-group">
-              <textarea
-                className="form-control"
-                name="message"
-                rows="5"
-                data-rule="required"
-                data-msg="Please write something for us"
-                placeholder="Message"
-              ></textarea>
-              <div className="validate"></div>
-            </div>
-            <div className="mb-3">
-              <div className="loading">Loading</div>
-              <div className="error-message"></div>
-              <div className="sent-message">
-                Your message has been sent. Thank you!
-              </div>
-            </div>
-            <div className="text-center">
-              <button type="submit">Send Message</button>
-            </div>
-          </form>
         </div>
       </div>
     </section>
